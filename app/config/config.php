70b0036c08a49e787c190a23badcd1a32b8a23e9
Md Aminur Islam <eng.aminurislam@gmail.com>
2020-08-11 00:28:33 +0600

    clean MVC framework by php

--- app/config/config.php
+++ app/config/config.php
@@ -1,16 +1,16 @@
 <?php
 //database config
-define('DB_NAME', 'oop_mvc');
 define('DB_HOST', 'localhost');
-define('DB_PASS', '');
-define('DB_USER', 'root');
+define('DB_NAME', '_YOUR_DB_NAME');
+define('DB_PASS', '_YOUR_DB_PASSWORD');
+define('DB_USER', '_YOUR_DB_USER_NAME');
 
 
 //app root
 define( 'APP_ROOT', dirname(dirname(__FILE__)) );
 
 //Site Root
-define( 'URL_ROOT', 'http://localhost/Endless/php-oop/php-light-mvc' );
+define( 'URL_ROOT', '_YOUR_PROJECT_ROOT_URL' );
 
 //site title
-define('SITE_TITLE', 'MVC Practice');
+define('SITE_TITLE', '_YOUR_PROJECT_TITLE');
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,15 +1,21 @@
 <?php
 class Pages extends Controller{
     public function __construct(){
-        $this->postModel = $this->model('Post');
+        
     }
 
     public function index(){
-        $posts = $this->postModel->getPosts();
         $data = [
-            'title' => 'Welcome to home page',
-            'posts' => $posts
+            'title' => 'Light MVC By PHP',
         ];
         $this->view('pages/index', $data);
+    }
+
+    public function about(){
+        $data = [
+            'title' => 'About Us'
+        ];
+
+        $this->view('pages/about', $data);
     }
 }
